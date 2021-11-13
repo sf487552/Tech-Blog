debd7a19be223cdc0d49c52ab5b13935b0579ed1
Steve Fokas <sf487552@gmail.com>
2021-11-12 23:32:17 -0500

    a;gh

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,19 +1,21 @@
-async function deleteFormHandler(event){
-    event.preventDefault();
-
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-
-    await fetch(`/api/posts/${id}`, {
-        method: 'DELETE'
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      } 
+async function deletePostHandler(event) {
+  event.preventDefault();
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  const response = await fetch("/api/posts/" + post_id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText); 
+  }
 }
-
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document.querySelector("#edit-btn").addEventListener("click", editPostHandler);
+document
+  .querySelector("#delete-btn")
+  .addEventListener("click", deletePostHandler);
